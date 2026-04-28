02f11adb8370e937d8bfb39c4602ca57ebfe5de5
mewcury-07 <marypayofelin78@gmail.com>
2026-04-28 20:21:28 +0800

    Add files via upload

---
+++ active-nav.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentPage = window.location.pathname.split("/").pop();
+  document.querySelectorAll("nav a").forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
